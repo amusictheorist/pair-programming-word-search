8bdfdc800957de0b933681dcf4ff69360e14d6f8
amusictheorist <a_millaresthomson@outlook.com>
2024-09-25 16:49:37 -0400

    First commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  let transposeMatrix = [];
+  for (let col = 0; col < matrix[0].length; col++) {
+    transposeMatrix[col] = [];
+    for (let row = 0; row < matrix.length; row++)
+      transposeMatrix[col].push(matrix[row][col]);
+  }
+  return transposeMatrix;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+    if (letters.length === 0 || word.length === 0 || typeof word !== "string") {
+        return false;
+    }
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    const lettersTranspose = transpose(letters);
+    const verticalJoin = lettersTranspose.map(ls => ls.join(''));
+    for (const l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+};
